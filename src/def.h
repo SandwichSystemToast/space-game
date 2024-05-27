9b394e30b620bbe7327c2fc953960a9f47a7a521
Stepan Moloshnev <cylapz@proton.me>
2024-05-27 20:57:57 +0300

    Made def.h crossplatform

--- src/def.h
+++ src/def.h
@@ -4,15 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long u64;
+#include <stdint.h>
 
-typedef char i8;
-typedef short i16;
-typedef int i32;
-typedef long i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 typedef size_t z;
 
