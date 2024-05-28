93197820d60a7566e8c22c8fc39c248811f33496
Stepan Moloshnev <cylapz@proton.me>
2024-05-28 14:48:53 +0300

    Add physac

--- src/def.h
+++ src/def.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define _XOPEN_SOURCE 700
+#define _POSIX_C_SOURCE 200809L
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
+#include "def.h"
+
 #include "raylib.h"
+
+#define PHYSAC_IMPLEMENTATION
+#include "physac/src/physac.h"
+#undef PHYSAC_IMPLEMENTATION
 
 int main(void)
 {
+  InitPhysics();
+
   InitWindow(800, 450, "raylib [core] example - basic window");
 
   while (!WindowShouldClose())
@@ -10,13 +18,15 @@ int main(void)
     ClearBackground(RAYWHITE);
     DrawText(
       "Congrats! You created your first window!",
-      190, 200, 20,
-      LIGHTGRAY
+      190, 200,
+      20, LIGHTGRAY
     );
     EndDrawing();
   }
 
   CloseWindow();
+
+  ClosePhysics();
 
   return 0;
 }
