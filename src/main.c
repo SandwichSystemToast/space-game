43a2f58b62f22d1d155691f19face2651b6b2be6
ktnlvr <artur.roos.artur@gmail.com>
2024-06-02 22:29:19 +0300

    Use appropriate phases for all systems

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "def.h"
 
+#include "flecs.h"
 #include "flecs/addons/flecs_c.h"
 #include "raylib.h"
 #include "raymath.h"
@@ -99,20 +100,19 @@ int main(void) {
   ECS_COMPONENT_DEFINE(world, c_physics_shape);
 
   // systems
+  ECS_SYSTEM(world, accept_input, EcsOnLoad, c_player_input($));
+  ECS_SYSTEM(world, begin_frame, EcsPostLoad, c_camera($));
+
+  ECS_SYSTEM(world, camera_follow, EcsPreUpdate, c_camera($));
+
   ECS_SYSTEM(world, move_player_character, EcsOnUpdate, c_player_character,
              c_transform, c_player_input($));
 
-  ECS_SYSTEM(world, accept_input, EcsOnUpdate, c_player_input($));
-  ECS_SYSTEM(world, camera_follow, EcsPreUpdate, c_camera($));
-  ECS_SYSTEM(world, begin_frame, EcsPreUpdate, c_camera($));
-
-  ECS_SYSTEM(world, solve_collisions, EcsOnUpdate);
-
-  ECS_SYSTEM(world, render_player, EcsOnUpdate, c_player_character,
+  ECS_SYSTEM(world, render_player, EcsPostUpdate, c_player_character,
              c_player_input($), c_camera($));
-  ECS_SYSTEM(world, render_shapes, EcsOnUpdate, c_transform, c_physics_shape);
+  ECS_SYSTEM(world, render_shapes, EcsPostUpdate, c_transform, c_physics_shape);
 
-  ECS_SYSTEM(world, end_frame, EcsPostUpdate);
+  ECS_SYSTEM(world, end_frame, EcsOnStore);
 
   // singletons
   ecs_singleton_set(world, c_player_input, {0});
